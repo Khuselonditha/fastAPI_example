0fc6b9f592d9347703e89c772f763086e9c65c53
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-19 10:54:58 +0200

    After adding the user route tests (users in db)

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,13 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.json(), output)
 
     
+    # Test users route
+    def test_users_pass(self):
+        response = self.client.get("/api/v1/users")
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
     
+
 if __name__ == "__main__":
     unittest.main()
