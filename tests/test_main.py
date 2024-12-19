b89cfa94d89e9436a468ccf052e10e40c37ab478
Khuselo Nditha <khuselonditha@gmail.com>
2024-12-19 11:08:03 +0200

    After adding the users route tests (no users in db)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,7 @@
 # imports
 import unittest
 from fastapi.testclient import TestClient
-from main import app
+from main import app, db
 from uuid import UUID
 
 
@@ -18,13 +18,25 @@ class TestAPI(unittest.TestCase):
         self.assertEqual(response.json(), output)
 
     
-    # Test users route
+    # Test users route (users in db)
     def test_users_pass(self):
         response = self.client.get("/api/v1/users")
         self.assertEqual(response.status_code, 200)
         self.assertIsInstance(response.json(), list)
+        self.assertEqual(len(db), 2)
 
+
+    # Test users route (no users in db)
+    def test_users_fail(self):
+        # Clear the database to simulate no users
+        db.clear()
+
+        response = self.client.get("/api/v1/users")
+        output = {"detail": "No users found in the database."}
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), output)
     
+
 
 if __name__ == "__main__":
     unittest.main()
